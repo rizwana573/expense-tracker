0de4c3ea075db851bc32dde901fab0cedcf9ca47
rizwana573 <mehar.rizwana573@gmail.com>
2025-11-12 13:14:49 +0530

    using controlled inputs for one-way data-binding

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,27 +1,41 @@
+import {useState} from "react";
+
 export const ExpenseForm = ({setExepenses}) => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [amount, setAmount] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const expense = {...getFormdata(e.target), id:crypto.randomUUID()}
+    const expense = {title,category, amount, id:crypto.randomUUID() }
+    setExepenses(prevState => [
+      ...prevState, expense
+    ]);
+    setTitle(" ");
+    setCategory(" ");
+    setAmount(" ");
+    /*const expense = {...getFormdata(e.target), id:crypto.randomUUID()}
     setExepenses(prevState => [...prevState, expense])
-    e.target.reset();
+     e.target.reset();
+     */
+   
   };
-  const getFormdata = (form) => {
+  /*const getFormdata = (form) => {
     const formData = new FormData(form);
     const data = {};
     for (const [key, value] of formData.entries()) {
       data[key] = value;
     }
     return data;
-  };
-
+  };*/
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" name="title" />
+        <input id="title" name="title" value={title} onChange= {e => setTitle(e.target.value)}/>
       </div>
       <div className="input-container">
-        <select id="category" name="category">
+        <select id="category" name="category" value={category} onChange = {e=> setCategory(e.target.value)}>
           <option value="" hidden>
             Select Category
           </option>
@@ -34,7 +48,7 @@ export const ExpenseForm = ({setExepenses}) => {
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" name="amount" />
+        <input id="amount" name="amount" value={amount} onChange={e=> setAmount(e.target.value)}/>
       </div>
       <button className="add-btn">Add</button>
     </form>
