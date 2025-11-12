e7f95f40405e63430af626d0342da26bbb79cd62
rizwana573 <mehar.rizwana573@gmail.com>
2025-11-12 15:09:04 +0530

    used useRef hook that lets you reference a value that's not needed for rendering

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,35 +1,42 @@
-import {useState} from "react";
+import { useState, useRef, useEffect } from "react";
 
-export const ExpenseForm = ({setExepenses}) => {
-  // const [title, setTitle] = useState('');
-  // const [category, setCategory] = useState('');
-  // const [amount, setAmount] = useState('');
+export const ExpenseForm = ({ setExepenses }) => {
+  /*/*const [expenseObj, setExpenseObj] = useState({
+    title: "",
+    category: "",
+    amount: "",
+  });*/
+  const titleRef= useRef(null);
+  const categoryRef= useRef(null);
+  const amountRef= useRef(null);
 
-  const [expenseObj, setExpenseObj] = useState({
-    title:"", 
-    category:"", 
-    amount:""
-  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setExepenses(prevState => [
-      ...prevState, {...expenseObj, id:crypto.randomUUID()} 
+    setExepenses((prevState) => [
+      ...prevState,
+      { title: titleRef.current.value,
+        category: categoryRef.current.value,
+        amount: amountRef.current.value,
+        id: crypto.randomUUID() },
+    ]);
+    /*setExepenses((prevState) => [
+      ...prevState,
+      { ...expenseObj, id: crypto.randomUUID() },
     ]);
     setExpenseObj({
-       title:"", 
-    category:"", 
-    amount:""
-    });
-    // setTitle(" ");
-    // setCategory(" ");
-    // setAmount(" ");
-    /*const expense = {...getFormdata(e.target), id:crypto.randomUUID()}
+      title: "",
+      category: "",
+      amount: "",
+    });*/
+
+    /*/*const expense = {...getFormdata(e.target), id:crypto.randomUUID()}
     setExepenses(prevState => [...prevState, expense])
      e.target.reset();
      */
-   
   };
-  /*const getFormdata = (form) => {
+
+  /*/*const getFormdata = (form) => {
     const formData = new FormData(form);
     const data = {};
     for (const [key, value] of formData.entries()) {
@@ -41,10 +48,32 @@ export const ExpenseForm = ({setExepenses}) => {
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" name="title" value={expenseObj.title} onChange= {e => setExpenseObj(prevState => ({...prevState, title:e.target.value}))}/>
+        <input
+          id="title"
+          name="title"
+          // value={expenseObj.title}
+          // onChange={(e) =>
+          //   setExpenseObj((prevState) => ({
+          //     ...prevState,
+          //     title: e.target.value,
+          //   }))
+          // }
+          ref={titleRef}
+        />
       </div>
       <div className="input-container">
-        <select id="category" name="category" value={expenseObj.category} onChange = {e=> setExpenseObj(prevState => ({...prevState, category:e.target.value}))}>
+        <select
+          id="category"
+          name="category"
+          // value={expenseObj.category}
+          // onChange={(e) =>
+          //   setExpenseObj((prevState) => ({
+          //     ...prevState,
+          //     category: e.target.value,
+          //   }))
+          // }
+           ref={categoryRef}
+        >
           <option value="" hidden>
             Select Category
           </option>
@@ -57,7 +86,18 @@ export const ExpenseForm = ({setExepenses}) => {
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" name="amount" value={expenseObj.amount} onChange={e=> setExpenseObj(prevState => ({...prevState, amount: e.target.value}))}/>
+        <input
+          id="amount"
+          name="amount"
+          // value={expenseObj.amount}
+          // onChange={(e) =>
+          //   setExpenseObj((prevState) => ({
+          //     ...prevState,
+          //     amount: e.target.value,
+          //   }))
+          // }
+           ref={amountRef}
+        />
       </div>
       <button className="add-btn">Add</button>
     </form>
