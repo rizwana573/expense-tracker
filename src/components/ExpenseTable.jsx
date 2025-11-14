443e50414a0bc7f6e91406dff38738302727943d
rizwana573 <mehar.rizwana573@gmail.com>
2025-11-14 22:40:53 +0530

    implemented sorting in the table

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import { useFilter } from "../hooks/useFilter";
 import { ContextMenu } from "./ContextMenu";
 
-export const ExpenseTable = ({ expense, setExpenses, setExpenseObj, setEditingRowId }) => {
+export const ExpenseTable = ({
+  expense,
+  setExpenses,
+  setExpenseObj,
+  setEditingRowId,
+}) => {
   const [menuPosition, setMenuPosition] = useState(null);
   const [filteredData, setQuery] = useFilter(expense, (data) => data.category);
   const [rowId, setRowId] = useState(null);
+  const [sortCallback, setSortCallback] = useState(() => ()=> {});
 
   const handleFilterChange = (e) => {
     setQuery(e.target.value);
@@ -16,11 +22,56 @@ export const ExpenseTable = ({ expense, setExpenses, setExpenseObj, setEditingRo
   );
   return (
     <>
-      <ContextMenu menuPosition={menuPosition} setMenuPosition={setMenuPosition} rowId={rowId} setExpenses={setExpenses} setExpenseObj={setExpenseObj} expense={expense} setEditingRowId={setEditingRowId}/>
-      <table className="expense-table" onClick={() => setMenuPosition({})}>
+      <ContextMenu
+        menuPosition={menuPosition}
+        setMenuPosition={setMenuPosition}
+        rowId={rowId}
+        setExpenses={setExpenses}
+        setExpenseObj={setExpenseObj}
+        expense={expense}
+        setEditingRowId={setEditingRowId}
+      />
+      <table
+        className="expense-table"
+        onClick={() => {
+          if (menuPosition?.left) {
+            setMenuPosition({});
+          }
+        }}
+      >
         <thead>
           <tr>
-            <th>Title</th>
+             <th className="amount-column">
+              <div>
+                <span>Title</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  viewBox="0 0 384 512"
+                  className="arrow up-arrow"
+                  fill="#fff"
+                  onClick={() => {
+                    setSortCallback(() => (a, b) => a.amount - b.amount);
+                  }}
+                >
+                  <title>Ascending</title>
+                  <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  viewBox="0 0 384 512"
+                  className="arrow down-arrow"
+                  fill="#fff"
+                  onClick={() => {
+                    setSortCallback(() => (a, b) => b.amount - a.amount);
+                  }}
+                >
+                  <title>Descending</title>
+                  <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                </svg>
+              </div>
+            </th>
             <th>
               <select onChange={handleFilterChange}>
                 <option value="">All</option>
@@ -40,6 +91,9 @@ export const ExpenseTable = ({ expense, setExpenses, setExpenseObj, setEditingRo
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
                   fill="#fff"
+                  onClick={() => {
+                    setSortCallback(() => (a, b) => a.amount - b.amount);
+                  }}
                 >
                   <title>Ascending</title>
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
@@ -50,6 +104,9 @@ export const ExpenseTable = ({ expense, setExpenses, setExpenseObj, setEditingRo
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
                   fill="#fff"
+                  onClick={() => {
+                    setSortCallback(() => (a, b) => b.amount - a.amount);
+                  }}
                 >
                   <title>Descending</title>
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
@@ -59,13 +116,13 @@ export const ExpenseTable = ({ expense, setExpenses, setExpenseObj, setEditingRo
           </tr>
         </thead>
         <tbody>
-          {filteredData.map(({ id, title, category, amount }) => {
+          {filteredData.sort(sortCallback).map(({ id, title, category, amount }) => {
             return (
               <tr
                 key={id}
                 onContextMenu={(e) => {
                   e.preventDefault();
-                  setMenuPosition({left:e.clientX, top: e.clientY});
+                  setMenuPosition({ left: e.clientX, top: e.clientY });
                   setRowId(id);
                 }}
               >
@@ -77,7 +134,12 @@ export const ExpenseTable = ({ expense, setExpenses, setExpenseObj, setEditingRo
           })}
           <tr>
             <th>Total</th>
-            <th></th>
+            <th
+              className="clear-sort"
+              onClick={()=>setSortCallback(() => () => {})}
+            >
+              Clear Sort
+            </th>
             <th>₹{total}</th>
           </tr>
         </tbody>
